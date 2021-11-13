f00ab7c7380aebf94c33b5df136853a9e695e6f1
utsav karmakar <91425188+UTSAVKARMAKAR@users.noreply.github.com>
2021-11-13 15:46:57 +0530

    class 9 project

--- sketch.js
+++ sketch.js
@@ -1,13 +1,39 @@
-var ball 
 
 function setup() {
-  createCanvas(400, 400);
-  ball = createSprite (200,200,10,10)
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
 
 }
 
-function draw() {
-  background(220);
-  drawSprites()
+function draw() 
+{
 
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  
+    background("red");
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  
+    background("blue");
+    
+  
+ 
+    if (keyIsDown(UP_ARROW)) 
+  
+    background("yellow");
+   
+  
+
+  if (keyIsDown(DOWN_ARROW)) 
+  
+    background("green");
+  
+
+
+  
+  drawSprites();
 }
+
